58866f7cac6805fe7f2913a8eb337ca0f56e08a1
Gabriel Marques Frahm <gabriel_frahm@hotmail.com>
2020-11-03 20:52:57 -0300

    finalizado

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,10 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionService {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 interface Balance {
   income: number;
   outcome: number;
@@ -15,14 +20,41 @@ class TransactionsRepository {
 
   public all(): Transaction[] {
     // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
     // TODO
+    const { income, outcome } = this.transactions.reduce(
+      (acc: Balance, transaction: Transaction) => {
+        // eslint-disable-next-line no-unused-expressions
+        transaction.type === 'income'
+          ? (acc.income += transaction.value)
+          : (acc.outcome += transaction.value);
+
+        return acc;
+      },
+
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+    const total = income - outcome;
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
+  public create({ title, value, type }: CreateTransactionService): Transaction {
     // TODO
+    const transaction = new Transaction({
+      title,
+      value,
+      type,
+    });
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
